cdecc4e2fe14c188123df2446945e853e503a6ca
Elvan H <elvanhd@gmail.com>
2021-12-27 22:26:47 +0700

    creating some first routes

--- app.js
+++ app.js
@@ -4,22 +4,30 @@ const express = require('express');
 
 const blogRoutes = require('./routes/blogRoutes');
 
+// Create an instance of express
 const app = express();
 
 // Activate EJS view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
-app.use(express.static('public')); // Serve static files (e.g. CSS files)
+// Parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
 
+// Serve static files (e.g. CSS files)
+app.use(express.static('public'));
+
+// Mount blog routes
 app.use(blogRoutes);
 
+// Default error handling function
+// Will become active whenever any route / middleware crashes
 app.use(function (error, req, res, next) {
-  // Default error handling function
-  // Will become active whenever any route / middleware crashes
   console.log(error);
   res.status(500).render('500');
 });
 
-app.listen(3000);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,7 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(302).redirect('/posts');
+});
+
+router.get('/posts', (req, res) => {
+  res.render('posts-list');
+});
+
+router.get('/new-post', (req, res) => {
+  res.render('create-post');
 });
 
 module.exports = router;
